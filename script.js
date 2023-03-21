d31531d9a4a3d54c8ddb0a7f9d99729c4d2e1de8
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-03-21 16:55:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,56 @@
-const g = document.querySelector('#good');
-const c = document.querySelector('#cheap');
-const f = document.querySelector('#fast');
-g.checked=true;
+// const g = document.querySelector('#good');
+// const c = document.querySelector('#cheap');
+// const f = document.querySelector('#fast');
 const tog = document.querySelectorAll('.toggle')
+let count = 0;
 let arr = Array.from(tog);
 arr.forEach((toggle)=>{
     toggle.addEventListener('click', (e)=>{
-		g.checked=false;
-        if(g.checked && c.checked){
-			if(f.checked){
-	            f.checked=false;
-			}
-        }
-        else if(g.checked){
-            if(f.checked){
-				if(c.checked){
-					c.checked=false;
-				}
-	            c.disabled=true;
-            }
-            else{
-				// c.checked=true;
-                c.disabled=false;
-            }
-        }
-        else if(c.checked){
-            if(f.checked){
-				if(g.checked){
-					g.checked=false;
-				}
-	            g.disabled=true;
-            }
-            else{
-				// g.checked=true;
-                g.disabled=false;
-            }
-        }
-        else{
-            toggle.disabled=false;
-        }
+        if(toggle.checked){
+			count++;
+		}
+		if(count>2){
+			toggle.checked=false;
+			count = 0;
+		}
+		toggle.disabled=false;
     })
 })
+
+
+
+
+
+
+// if(g.checked && c.checked){
+// 			if(f.checked){
+// 	            f.checked=false;
+// 			}
+//         }
+//         else if(g.checked){
+//             if(f.checked){
+// 				if(c.checked){
+// 					c.checked=false;
+// 				}
+// 	            c.disabled=true;
+//             }
+//             else{
+// 				// c.checked=true;
+//                 c.disabled=false;
+//             }
+//         }
+//         else if(c.checked){
+//             if(f.checked){
+// 				if(g.checked){
+// 					g.checked=false;
+// 				}
+// 	            g.disabled=true;
+//             }
+//             else{
+// 				// g.checked=true;
+//                 g.disabled=false;
+//             }
+//         }
+//         else{
+//             toggle.disabled=false;
+//         }
