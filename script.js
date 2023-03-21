6ea5c3a27cc739413c5232bd0d545d4beb8706f5
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-03-21 16:18:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+const g = document.querySelector('#good');
+const c = document.querySelector('#cheap');
+const f = document.querySelector('#fast');
+
+const tog = document.querySelectorAll('.toggle')
+let arr = Array.from(tog);
+arr.forEach((toggle)=>{
+    toggle.addEventListener('click', (e)=>{
+        if(g.checked && c.checked){
+            f.checked=false;
+        }
+        else if(g.checked){
+            if(f.checked){
+                c.disabled=true;
+            }
+            else{
+                c.disabled=false;
+            }
+        }
+        else if(c.checked){
+            if(f.checked){
+                g.disabled=true;
+            }
+            else{
+                g.disabled=false;
+            }
+        }
+        else{
+            toggle.disabled=false;
+        }
+    })
+})
